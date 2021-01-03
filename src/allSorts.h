d56e40f31806491d1175f019dc77e09c66579eca
David Weir <weird4@mailhost1.computing.dcu.ie>
2021-01-03 17:37:03 +0000

    Updated allSorts to include mergesort algorithm

--- src/allSorts.h
+++ src/allSorts.h
@@ -31,3 +31,62 @@ void s_sort(int arr[], int len)
     }
 
 }
+
+void merge(int arr[], int left, int mid, int right)
+{
+    int i, j, start;
+    int mem1 = mid - left + 1;
+    int mem2 = right - mid;
+ 
+    // create temp arrays
+    int lst1[mem1], lst2[mem2];
+ 
+    // Copy data to temp arrays lst1[] and lst2[]
+    for (i = 0; i < mem1; i++)
+        lst1[i] = arr[left + i];
+    for (j = 0; j < mem2; j++)
+        lst2[j] = arr[mid + 1 + j];
+ 
+    i = 0;
+    j = 0;
+    start = left; 
+    while (i < mem1 && j < mem2) {
+        if (lst1[i] <= lst2[j]) {
+            arr[start] = lst1[i];
+            i++;
+        }
+        else {
+            arr[start] = lst2[j];
+            j++;
+        }
+        start++;
+    }
+
+    //copies remaining elements of both subarrays to the merged array
+    while (i < mem1) {
+        arr[start] = lst1[i];
+        i++;
+        start++;
+    }
+ 
+    while (j < mem2) {
+        arr[start] = lst2[j];
+        j++;
+        start++;
+    }
+}
+ 
+void mergesort(int arr[], int left, int right)
+{
+    if (left < right) {
+
+        int mid = left + (right - left) / 2;
+ 
+        // Sort first and second halves
+        mergesort(arr, left, mid);
+        mergesort(arr, mid + 1, right);
+ 
+        merge(arr, left, mid, right);
+    }
+}
+ 
