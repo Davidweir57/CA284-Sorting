b54217f29636b473df1cf2963f5530edaeb4128d
David Weir <weird4@mailhost1.computing.dcu.ie>
2021-01-06 17:16:25 +0000

    Memory bug fix

--- src/mergesort.c
+++ src/mergesort.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 void merge(int arr[], int l, int m, int r)
 {
     int i, j, k;
@@ -5,8 +6,10 @@ void merge(int arr[], int l, int m, int r)
     int n2 = r - m;
  
     /* create temp arrays */
-    int L[n1], R[n2];
- 
+    //int L[n1], R[n2];
+    int* L = malloc((n1) * sizeof(int));
+    int* R = malloc((n2) * sizeof(int));
+
     /* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
